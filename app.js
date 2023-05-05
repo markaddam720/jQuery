0cf357e04fb6706a733ae2c00a3ea05b29236d59
Mark Curtiss <mark.curtiss0212@stu.mtec.edu>
2023-05-05 17:51:08 -0600

    Followed intructions on instruction.txt and edited index.html, app.js.

---
+++ app.js
@@ -0,0 +1,11 @@
+$('.navItem').css('color','white');
+
+$('#nav').css('background-color','#145');
+
+let even = $('li').even();
+$(even).css('background-color','#ccc');
+
+let odd = $('li').odd();
+$(odd).css('background-color','#aaa');
+
+$('input').val('Mark');
